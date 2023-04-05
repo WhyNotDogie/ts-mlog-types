1e8a23228b439f1e396d5b1ff5a1cd1e84e74b30
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2023-04-05 16:06:43 +0200

    more typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,17 @@
+/**
+ * TSTMLOG special functions/properties
+ */
 declare namespace mlog {
     /**
      * # Do not use unless you know what you are doing!
      * Input raw MLOG to be put into the generated MLOG.
-     * @deprecated
-     * Do not use!!! Instead, use the api.
+     * Use the api instead of this.
+     * ```
+     * npm i -D ts-mlog-api
+     * ```
      * @param mlogInput The MLOG input string.
      */
-    function raw(mlogInput:string):void;
+    function raw(mlogInput:string): void;
     /**
      * # Do not use ~~unless you know what you are doing~~ AT ALL!
      * Input raw MLOG to be put into the generated MLOG at the specified line.
@@ -15,5 +20,21 @@ declare namespace mlog {
      * Do not EVER use!!!
      * @param mlogInput The MLOG input string.
      */
-    function raw(mlogInput:string):void;
+    function raw(mlogInput:string): void;
+    /**
+     * If the compiler is generating world processor code or not.
+     */
+    const isWorld: boolean;
+    /**
+     * All of the available versions.
+     */
+    type versions = {
+        "6":"v6"
+        "7":"v7"
+        "8":"v8"
+    }
+    /**
+     * The current version that is being compiled.
+     */
+    const version: keyof versions;
 }
